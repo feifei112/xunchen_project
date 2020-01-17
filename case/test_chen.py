e5c6a5e0a7e168051077bc53d9c2297a3159ba16
nui <1300458082@qq.com>
2020-01-17 14:23:19 +0800

    测试代码 进行优化操作

--- case/test_chen.py
+++ case/test_chen.py
@@ -20,10 +20,11 @@ class TestChen(object):
         # 登录按钮码
         self.factor_page.login_page().click_login_reg()
         # 输入手机号码
-        self.factor_page.mine_page().input_phone_log("13288834897")
-        # 输入密码
-        self.factor_page.mine_page().input_pwd_log("w123456789")
-        # 点击登录
-        self.factor_page.mine_page().click_log()
-        #点击确认
-        self.factor_page.mine_page().click_confirm_btn()
+        # self.factor_page.mine_page().input_phone_log("13288834897")
+        # # 输入密码
+        # self.factor_page.mine_page().input_pwd_log("13288834897")
+        # # 点击登录
+        # self.factor_page.mine_page().click_log()
+        # #点击确认
+        # self.factor_page.mine_page().click_confirm_btn()
+        self.factor_page.mine_page().account_login("13288834897","13288834897")
--- page/mine_page.py
+++ page/mine_page.py
@@ -12,14 +12,21 @@ class MinePage(BasePage):
     confirm_btn = page.confirm_btn  # 确认
 
     # 输入 手机号码
-    def input_phone_log(self,phone):
-        self.base_input_short(self.phone_log,phone)
-    # 输入密码
-    def input_pwd_log(self,pwd):
-        self.base_input_short(self.pwd_log,pwd)
-    # 点击登录
-    def click_log(self):
+    # def input_phone_log(self,phone):
+    #     self.base_input_short(self.phone_log,phone)
+    # # 输入密码
+    # def input_pwd_log(self,pwd):
+    #     self.base_input_short(self.pwd_log,pwd)
+    # # 点击登录
+    # def click_log(self):
+    #     self.base_click_element(self.log)
+    # # 点击确认
+    # def click_confirm_btn(self):
+    #     self.base_click_element(self.confirm_btn)
+
+    # 登录帐号流程
+    def account_login(self,phone,pwd):
+        self.base_input_short(self.phone_log, phone)
+        self.base_input_short(self.pwd_log, pwd)
         self.base_click_element(self.log)
-    # 点击确认
-    def click_confirm_btn(self):
         self.base_click_element(self.confirm_btn)
