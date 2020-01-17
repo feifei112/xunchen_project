c523dc16f03b09014c17b864f07e672391036f54
nui <1300458082@qq.com>
2020-01-17 14:27:07 +0800

    测试代码 进行优化操作

--- case/test_chen.py
+++ case/test_chen.py
@@ -22,9 +22,9 @@ class TestChen(object):
         # 输入手机号码
         # self.factor_page.mine_page().input_phone_log("13288834897")
         # # 输入密码
-        # self.factor_page.mine_page().input_pwd_log("13288834897")
+        # self.factor_page.mine_page().input_pwd_log("w123456789")
         # # 点击登录
         # self.factor_page.mine_page().click_log()
         # #点击确认
         # self.factor_page.mine_page().click_confirm_btn()
-        self.factor_page.mine_page().account_login("13288834897","13288834897")
+        self.factor_page.mine_page().account_login("13288834897","w123456789")
--- page/__init__.py
+++ page/__init__.py
@@ -15,8 +15,8 @@ login_reg =(By.ID,"com.bjcsxq.chat.carfriend:id/mine_username_tv")   # 登录按
 phone_log = (By.ID,"com.bjcsxq.chat.carfriend:id/login_phone_et") # 手机号码
 pwd_log = (By.ID,"com.bjcsxq.chat.carfriend:id/login_pwd_et") #密码
 log = (By.ID,"com.bjcsxq.chat.carfriend:id/login_btn") # 登录
-confirm_btn = (By.ID,"com.bjcsxq.chat.carfriend:id/btn_neg ") # 确认
-
+# confirm_btn = (By.ID,"com.bjcsxq.chat.carfriend:id/btn_neg ") # 确认
+confirm_btn = (By.XPATH,"//*[contains(@text,'确定')]")
 
 
 
